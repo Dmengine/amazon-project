49fc20f47b4edd1768b1d1f1997170b36bde5163
Dmengine <azeezoladimeji23@gmail.com>
2024-09-28 17:59:17 +0100

    project complete

--- data/cart-class.js
+++ data/cart-class.js
@@ -1,5 +1,5 @@
 class Cart {
-  cartItem;
+  cartItems;
   #localStorageKey;
 
   constructor(localStorageKey) {
@@ -9,7 +9,7 @@ class Cart {
 
   #loadFromStorage() {
     this.cartItems = JSON.parse(localStorage.getItem(this.#localStorageKey));
-  
+
     if (!this.cartItems) {
       this.cartItems = [{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -26,62 +26,61 @@ class Cart {
   saveToStorage() {
     localStorage.setItem(this.#localStorageKey, JSON.stringify(this.cartItems));
   }
-  
+
   addToCart(productId) {
     let matchingItem;
-  
+
     this.cartItems.forEach((cartItem) => {
       if (productId === cartItem.productId) {
         matchingItem = cartItem;
       }
     });
-  
+
     if (matchingItem) {
       matchingItem.quantity += 1;
     } else {
-      cart.push({
+      this.cartItems.push({
         productId: productId,
         quantity: 1,
         deliveryOptionId: '1'
       });
     }
-  
+
     this.saveToStorage();
   }
 
   removeFromCart(productId) {
     const newCart = [];
-  
-    this.cartItem.forEach((cartItem) => {
+
+    this.cartItems.forEach((cartItem) => {
       if (cartItem.productId !== productId) {
         newCart.push(cartItem);
       }
     });
-  
+
     this.cartItems = newCart;
-  
+
     this.saveToStorage();
   }
 
   updateDeliveryOption(productId, deliveryOptionId) {
     let matchingItem;
-  
+
     this.cartItems.forEach((cartItem) => {
       if (productId === cartItem.productId) {
         matchingItem = cartItem;
       }
     });
-  
+
     matchingItem.deliveryOptionId = deliveryOptionId;
-  
+
     this.saveToStorage();
   }
 }
 
-
-export const cart = new Cart('cart-oop');
-
-const bussinessCart = new Cart('bussiness-cart-oop');
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
 
 console.log(cart);
-console.log(bussinessCart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart);
