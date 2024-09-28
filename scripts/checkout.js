d882f044772e9045913f43b88de0aa0985e72d6a
Dmengine <azeezoladimeji23@gmail.com>
2024-09-28 15:29:25 +0100

    loadProducts from backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,10 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 // import '../data/cart-class.js';
-import '../data/car.js';
+// import '../data/car.js';
+import { loadProducts } from '../data/products.js';
 
-
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts( () => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
